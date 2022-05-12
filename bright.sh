3e5237af04d925e8c27ffc82f6e01a6cf0b59935
Everything on Arm <92229936+D2Lx0wse@users.noreply.github.com>
2022-05-12 10:58:53 +0200

    Make brightshell detect current brightness
    Also a warning

--- bright.sh
+++ bright.sh
@@ -2,6 +2,10 @@ cd /sys/class/backlight
 bldir=$( ls )
 cd $bldir
 max=$( cat max_brightness)
+cbr=$( cat brightness)
+echo If your backlight turns off type up arrow enter last value you had and enter again
+echo Current brightness is $cbr
+echo If you have full brightness on your gui consider that your max value
 echo Set brightness value between 0 and $max
 read bright
 echo $bright > brightness
